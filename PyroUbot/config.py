15deed386572d45569f1c054e6707b5d7153b6dc
victor3232 <44395332+victor3232@users.noreply.github.com>
2024-11-05 05:08:09 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -7,13 +7,13 @@ load_dotenv(".env")
 API_ID: int = os.getenv('API_ID', 23346063)
 API_HASH: str = os.getenv('API_HASH', "91a8bfefb69664b2770d41c9876e052e")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7615008031:AAFbKJ47-GYjyornEuRtOkZjpaW6RWLAPM8")
 
-OWNER_ID = int(os.getenv("OWNER_ID"))
+OWNER_ID = int(os.getenv("1835508209"))
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT"))
+LOGS_MAKER_UBOT = int(os.getenv("-1001948756607"))
 
-MAX_BOT = int(os.getenv("MAX_BOT"))
+MAX_BOT = int(os.getenv("30"))
 
 RMBG_API = os.getenv("RMBG_API")
 
